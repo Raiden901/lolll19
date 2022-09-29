fab2c81d721cab339312b62355c6c8835eb7c863
Raiden901 <104097690+Raiden901@users.noreply.github.com>
2022-09-29 19:38:36 +0100

    Initial commit

---
+++ footer.php
@@ -0,0 +1,35 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>روابط سريعة</h3>
+         <a href="home.php"> الفصحةالرئيسية</a>
+
+         <a href="shop.php">السوق</a>
+         <a href="contact.php">تواصل معنا </a>
+      </div>
+
+      <div class="box">
+         <h3>روابط إيضافية</h3>
+         <a href="login.php">تسجيل دخول </a>
+         <a href="register.php">إنشاء حساب </a>
+         <a href="cart.php">عربة التسوق</a>
+         <a href="orders.php">طلباتك</a>
+      </div>
+
+
+
+      <div class="box">
+         <h3> تابعنا عبر صفحاتنا </h3>
+         <a href="https://www.facebook.com/Aurora-اورورا-107178791018296/"> <i class="fab fa-facebook-f"></i> facebook </a>
+
+         <a href="https://www.instagram.com/aurora1.z/"> <i class="fab fa-instagram"></i> instagram </a>
+>
+      </div>
+
+   </div>
+
+
+
+</section>
